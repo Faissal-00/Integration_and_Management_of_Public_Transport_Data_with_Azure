f656f789a4728c335fe55522a6ba12b2bc85f6cc
Faissal-00 <Faissal-00>
2023-09-22 13:41:21 +0000

    Loadingdata

---
+++ .ipynb_checkpoints/ETL.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+#Configure Spark to access Azure Data Lake Storage Gen2 using the account key
+spark.conf.set(
+    f"fs.azure.account.key.faissalmoufllastorage.dfs.core.windows.net", 
+    "MCuRaZ+x1FDZbtFgWoDFu7u9D2EpjNPFoyGSgfGbnYs03Qsvdua+2sUNzTLpNv7e2liaOXz9iEGC+AStYx6O9w=="
+)                                                               
+
+# COMMAND ----------
+
+## List the contents of the 'raw' folder in Azure Data Lake Storage Gen2
+dbutils.fs.ls("abfss://publictransportdata@faissalmoufllastorage.dfs.core.windows.net/raw/")
+
+# COMMAND ----------
+
+#Set data lake file location
+file_location = "abfss://publictransportdata@faissalmoufllastorage.dfs.core.windows.net/raw/"
+#read in the data to dataframe df
+df = spark.read.format("csv").option("inferSchema", "True").option("header",
+"True").option("delimeter",",").load(file_location)
+#display the dataframe
+display(df)  
